315a2da3b9c8a0dbe4c4b8912fd7db81b22738c0
danilo90lan <15630@coderacademy.edu.au>
2024-11-23 09:36:02 +1100

    feat: increase or decrease pokemon card, API cal

--- react-events/src/App.js
+++ react-events/src/App.js
@@ -1,5 +1,6 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import PokemonCard from './components/pokemonCard';
 
 export default class App extends Component {
   constructor() {
@@ -15,24 +16,54 @@ export default class App extends Component {
   }
 
   IncreasePokemonNumber = () => {
-    this.setState({numberOfPokemon: this.state.numberOfPokemon + 1});
+    this.setState({ numberOfPokemon: this.state.numberOfPokemon + 1 });
   }
 
   decreasePokemonNumber = () => {
-    this.setState({numberOfPokemon: this.state.numberOfPokemon - 1});
+    if (this.state.numberOfPokemon > 0) {
+      this.setState({ numberOfPokemon: this.state.numberOfPokemon - 1 });
+    }
+
+  }
+
+  iterate() {
+    for (let i = 0; i < this.state.numberOfPokemon; i++) {
+      return <PokemonCard/>
+    }
   }
 
   render() {
     return (
       <>
-      <h1>Hello world</h1>
-      <h4>Number of Pokemon {this.state.numberOfPokemon}</h4>
-      <button onClick={
-        this.decreasePokemonNumber
-      }>Increase Pokemon Amount</button>
-      <button onClick={
-        this.IncreasePokemonNumber
-      }>Decrease Pokemon amount</button>
+        <h1>Hello world</h1>
+        <h4>Number of Pokemon {this.state.numberOfPokemon}</h4>
+        <button onClick={
+          this.decreasePokemonNumber
+        }>Decrease Pokemon Amount</button>
+        <button onClick={
+          this.IncreasePokemonNumber
+        }>Increase Pokemon amount</button>
+        { 
+        // creates an array of given length
+          Array(this.state.numberOfPokemon)
+          // fill the array with empty values
+          .fill(null)
+          // iterates over each elelemnt of the array and return the pokemonCard element
+          .map((element, index) => {
+            return <PokemonCard/>
+          })
+        }
+
+        {/* {
+          this.iterate()
+        } */}
+        
+
+        <PokemonCard/>
+        <button onClick={ () => {
+          this.IncreasePokemonNumber();
+          this.exampleFunc1();
+        }}>Random button</button>
       </>
     )
   }
---
+++ react-events/src/components/pokemonCard.jsx
@@ -0,0 +1,46 @@
+import React, {Component} from "react";
+
+export default class PokemonCard extends Component {
+    constructor() {
+        super();
+
+        this.state= {
+            pokemonName: "",
+            pokemonImage: ""
+        }
+    }
+    async componentDidMount() {
+        let randomNumber = Math.ceil(Math.random() * 1025);
+        let response = await fetch(`http://pokeapi.co/api/v2/pokemon/${randomNumber}`);
+        let data = await response.json();
+
+        this.setState({
+            pokemonName: data.name,
+            pokemonImage: data.sprites.front_default
+        })
+    }
+
+    render() {
+            return (
+                //group element in block
+                    
+                    <div className="pokemonCard">
+                        {/* //receive data from API
+                        //We can create a loading content (Conditional rendering) */}
+                        {
+                            //ternary operation
+                            (this.state.pokemonImage && this.state.pokemonName) ?
+                            //render content here if it's true 
+                            <>
+                                <h1>{this.state.pokemonName}</h1>
+                                <img src = {this.state.pokemonImage}></img>
+                            </>
+                            :
+                            // if it's false render this content
+                            <h1>LOADING</h1>
+                        }
+                    </div>
+            )
+    }
+
+}
